6f1d0de0e6e0caf681b9e16489a73caaf58d2c8a
Kelve Morais <53889581+NoodleEdit@users.noreply.github.com>
2025-01-18 11:22:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let inputSequence = "";
         document.addEventListener('keydown', function(event) {
             inputSequence += event.key;
             if (inputSequence === secretCode) {
-               window.open('https://c.wallhere.com/photos/7e/7a/anime_Hatsune_Miku_skirt_striped_panties_thigh_high_boots-1554631.jpg!d');
+               window.open('https://www.youtube.com/watch?v=_-2dIuV34cs');
                 inputSequence = "";
             }
            
